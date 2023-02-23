82f685858ed3b374af28231661e16e35dfb0e093
Marcus Lee <marcustutorial@hotmail.com>
2023-02-23 09:28:05 +0800

    added vim-visual-multi

--- lua/marcustut/packer.lua
+++ lua/marcustut/packer.lua
@@ -102,6 +102,7 @@ return require('packer').startup(function(use)
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     })
+    use('mg979/vim-visual-multi')
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
