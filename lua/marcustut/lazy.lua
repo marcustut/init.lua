b5ca43140b8b597a8f89fbf6b6dc215a3dff8049
Marcus Lee <marcustutorial@hotmail.com>
2024-01-29 14:31:04 +0000

    feat: added remote-nvim dep

--- lua/marcustut/lazy.lua
+++ lua/marcustut/lazy.lua
@@ -35,6 +35,18 @@ require("lazy").setup({
         config = true
     },
 
+    -- SSH
+    {
+        "amitds1997/remote-nvim.nvim",
+        version = "*",                  -- Pin to GitHub releases
+        dependencies = {
+            "nvim-lua/plenary.nvim",    -- For standard functions
+            "MunifTanjim/nui.nvim",     -- To build the plugin UI
+            "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+        },
+        config = true,
+    },
+
     -- Colorschemes
     {
         "catppuccin/nvim",
