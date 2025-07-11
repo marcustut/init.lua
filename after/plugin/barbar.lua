8fccc6c2e94a4ef5c742c329461fe04e98248e68
marcustut <marcustutorial@hotmail.com>
2025-07-11 14:31:15 +0800

    feat: added keymaps for barbar.nvim

---
+++ after/plugin/barbar.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<leader>bn", "<Cmd>BufferNext<CR>", { desc = "[B]uffer [N]ext" })
+vim.keymap.set("n", "<leader>bp", "<Cmd>BufferPrevious<CR>", { desc = "[B]uffer [P]revious" })
+vim.keymap.set("n", "<leader>bs", "<Cmd>BufferPick<CR>", { desc = "[B]uffer [S]elect" })
+vim.keymap.set("n", "<leader>bk", "<Cmd>BufferPickDelete<CR>", { desc = "[B]uffer [K]ill" })
